4ca9b236ebc19b8d4bc700d30c56aafdaaf68f16
Roberto C. Navarro <rnavarro70@gmail.com>
2018-12-01 11:46:48 -0600

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="div-board">
+          <div className="row">
+            <Note />
+          </div>
+        </div>
+        <div>
+          <h3>Create a Note</h3>
+          <label>Title:</label>
+          <input type="text" />
+          <br />
+          <label>Body:</label>
+          <textarea rows="5" />
+          <br />
+          <button className="btn btn-success add-button">Add</button>
+        </div>
       </div>
     );
   }
+}
+
+function Note() {
+  return (
+    <div className="col-sm-6">
+      <div className="card card-view">
+        <div className="card-body">
+          <h5 className="card-title">Note Title</h5>
+          <p>This is a very long note that we just made up</p>
+          <button className="btn btn-info">Edit</button>
+          <button className="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
